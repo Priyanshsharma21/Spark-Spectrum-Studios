2d0f5af49a49481a8707cb751e76ce9f3cdb7740
Priyanshsharma21 <piyuindi220@gmail.com>
2024-07-28 23:19:16 +0530

    big fixes

--- src/components/GridLine.jsx
+++ src/components/GridLine.jsx
@@ -7,21 +7,21 @@ const GridLine = ({ type, count }) => {
     <>
         <motion.div className="pt-hl-raster-h"
         whileInView={{ y: 0, opacity: 1 }}
-        initial={{ y: 20, opacity: 0 }}
+        initial={{ y: 2, opacity: 0 }}
         transition={{ duration: 0.5, ease: 'easeOut', delay: 0.05 }}
         >
 
         
   <motion.div className="pt-hl-raster-v"
     whileInView={{ y: 0, opacity: 1 }}
-    initial={{ y: 20, opacity: 0 }}
+    initial={{ y: 2, opacity: 0 }}
     transition={{ duration: 0.5, ease: 'linear', delay: 0.05 }}
   >
    {Array.from({length : 44}, (_, i)=>(
     <motion.div
         whileInView={{ y: 0, opacity: 1 }}
         initial={{ y: 100, opacity: 0 }}
-        transition={{ duration: 1, ease: 'linear', delay: 0.005 * i }}
+        transition={{ duration: 1, ease: 'linear' }}
         className="pt-raster-line-v"
         style={{ transform: "matrix(1, 0, 0, 1, 0, 0)" }}
     />
@@ -31,7 +31,7 @@ const GridLine = ({ type, count }) => {
 {Array.from({length : 12}, (_, i)=>(
     <motion.div
         whileInView={{ x: 0, opacity: 1 }}
-        initial={{ x: 50, opacity: 0 }}
+        initial={{ x: 2, opacity: 0 }}
         transition={{ duration: 1, ease: 'linear', delay: 0.005 * i }}
         className="pt-raster-line-h"
     />
